5bfd66c07db3c3872634d3ab4dbc446b1a6fc025
AnnKoh <annkoh12@gmail.com>
2020-07-02 00:11:55 +0800

    updating files

--- db/migrate/20200701150644_add_multiple_columns_to_users.rb
+++ db/migrate/20200701150644_add_multiple_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddMultipleColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :role, :integer
+    add_column :users, :role, :integer, default: 0
   end
 end
