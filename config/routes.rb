a87c8163dca7fbc5da083a1a36203c455794547c
AnnKoh <annkoh12@gmail.com>
2020-07-04 16:49:50 +0800

    save changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: 'pages#home'
+
+  resources :sessions, only: [:create, :destroy]
 
   resources :classrooms, only: [:show] do
     resources :work_groups, only: [:index, :show, :new, :create] do
