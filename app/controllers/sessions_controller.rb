ba887fc7270033a1cca5b4ec92882e0a5b15fe8d
AnnKoh <annkoh12@gmail.com>
2020-07-10 21:59:07 +0800

    jwt for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,27 @@
 class SessionsController < ApplicationController
   def create
-    user = User.where(email: params[:email]).first
+    user = User.find_by_email(params[:email])
 
-    if user.valid_password?(params[:password])
-      render json: user.as_json(only: [:email]), status: :created
+    if user && user.valid_password?(params[:password])
+      @current_user = user
+
+      render json: {
+        id: @current_user.id,
+        name: @current_user.name,
+        role: @current_user.role,
+        token: generate_jwt(@current_user.id)
+      }
     else
-      head(:unauthorized)
+      render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
     end
   end
 
-  def destroy
+  private
 
+  def generate_jwt(user_id)
+    JWT.encode({id: user_id,
+                exp: 60.days.from_now.to_i},'secret')
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
+  end
+
   root to: 'pages#home'
 
-  resources :sessions, only: [:create, :destroy]
+
+  resources :user, only: [:show, :update]
 
   resources :classrooms, only: [:show] do
     resources :work_groups, only: [:index, :show, :new, :create] do
