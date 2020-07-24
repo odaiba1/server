89a2704098e3029b091129cad21a6cad1750c3e9
Paulo DAlberti <phdalberti@gmail.com>
2020-07-24 17:03:17 +0900

    classroom policy

--- app/policies/classroom_policy.rb
+++ app/policies/classroom_policy.rb
@@ -1,27 +1,38 @@
 class ClassroomPolicy < ApplicationPolicy
-  # admin all permit
-  # teacher all permit for own classrooms
-  # students permit only show and index
   def index?
     case @user.role
     when 'admin' then true
-    when 'teacher'
-
-    when 'student'
-
-    else
-      false
+    when 'teacher' then @record.user_id == @user.id
+    when 'student' then @record.students.include?(@user)
+    else false
     end
   end
 
   def create?
-
+    %w[admin teacher].include?(@user.role)
   end
 
   def update?
-
+    case @user.role
+    when 'admin' then true
+    when 'teacher' then @record.user_id == @user.id
+    else false
+    end
   end
 
   alias show? index?
-  # alias :new?, :create?
+  alias new? create?
+  alias edit? update?
+  alias delete? update?
+
+  class ClassroomScope < Scope
+    def resolve
+      case @user.role
+      when 'admin' then @scope.all
+      when 'teacher' then @scope.where(user_id: @user.id)
+      when 'student' then @scope.where(id: @user.classrooms.ids)
+      else @scope.where(id: -1)
+      end
+    end
+  end
 end
