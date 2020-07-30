35708da7ffd7d466bf09108d6ba5de36c9e48a35
Paulo DAlberti <phdalberti@gmail.com>
2020-07-30 22:33:57 +0900

    verify authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  rescue_from StandardError,                with: :internal_server_error
+  # rescue_from StandardError,                with: :internal_server_error
   rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
