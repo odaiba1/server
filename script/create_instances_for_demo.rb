a2c3a0f6bbc887ceca7be456443531068ce6563b
Paulo DAlberti <phdalberti@gmail.com>
2020-09-01 23:44:24 +0900

    script for demo

---
+++ script/create_instances_for_demo.rb
@@ -0,0 +1,69 @@
+puts 'Welcome to instance creation script'
+puts 'Please write names of members who will participate on demo'
+puts 'Please separate all names by a space'
+puts '(e.g.: Paulo Ivan David)'
+print '> '
+users = gets.chomp.split(' ').map(&:capitalize)
+new_users = []
+
+users.each do |name|
+  user = User.find_by_name(name)
+  next if user
+
+  new_user = User.new(name: name, email: name + '@gmail.com', password: 'secret')
+  new_user.save!
+  new_users << new_user
+end
+
+users = users.map { |name| User.find_by_name(name) }
+
+new_users.each do |user|
+  StudentClassroom.create!(user: user, classroom: Classroom.first)
+end
+
+puts "Choose turn time in minutes for #{users.size} users"
+print '> '
+turn_time = gets.chomp.to_i
+
+puts 'Choose in how many minutes the lesson should start'
+print '> '
+start_at = gets.chomp.to_i
+
+WorkGroup.create!(
+  name: users.join('-'),
+  video_call_code: 'abc',
+  classroom: Classroom.first,
+  session_time: turn_time * 60_000 * users.size,
+  turn_time: turn_time * 60_000,
+  aasm_state: 'in_progress',
+  start_at: Time.now + start_at.minutes
+)
+
+users.each_with_index do |user, idx|
+  StudentWorkGroup.create!(
+    student: user,
+    work_group: WorkGroup.last,
+    joined: true,
+    turn: (idx % users.size).zero?
+  )
+end
+
+puts 'Paste url for worksheet template'
+print '> '
+image_url = gets.chomp
+
+WorksheetTemplate.create!(
+  title: 'Demo Template',
+  user: User.first,
+  image_url: image_url
+)
+
+Worksheet.create!(
+  title: 'Demo Worksheet',
+  canvas: '',
+  worksheet_template: WorksheetTemplate.last,
+  work_group: WorkGroup.last,
+  template_image_url: image_url
+)
+
+p 'finished'
