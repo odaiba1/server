4abc58ab9021a0aad7e54ab9db80f4e3623ef4db
Paulo DAlberti <phdalberti@gmail.com>
2020-09-05 15:19:49 +0900

    cloudinary uploader

--- app/services/cloudinary_uploader.rb
+++ app/services/cloudinary_uploader.rb
@@ -1,3 +1,15 @@
 class CloudinaryUploader
+  def initialize(image_url, photo)
+    @image_url = image_url
+    @photo = photo
+  end
 
+  def call
+    if @image_url&.include?('res.cloudinary.com/')
+      @image_url
+    elsif @image_url || @photo
+      source = @image_url || @photo
+      Cloudinary::Uploader.upload(source)
+    end
+  end
 end
