840320e2611bdac66915021a251cb3545240e9f0
Paulo DAlberti <phdalberti@gmail.com>
2020-09-08 21:54:51 +0900

    test mailer

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,7 +1,9 @@
 class InvitationMailer < ApplicationMailer
-  def demo_invite(users)
-    users.each do |user|
-      mail(to: user.email, subject: 'demo')
-    end
+  def demo_invite
+    @user = params[:user]
+    work_group = params[:work_group]
+    @one_time_password = rand(36**10).to_s(36)
+    @session_url = "https://odaiba-app.netlify.app/classrooms/#{work_group.classroom_id}/work_groups/#{work_group.id}"
+    mail(to: @user.email, subject: 'demo') if @user.update(password: @one_time_password)
   end
 end
