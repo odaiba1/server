371892aef9c6dc441bf8e0cdd947d37d8fdb331a
Frances Cheng <francesvcheng@gmail.com>
2020-09-15 13:56:44 -0700

    testing preview

--- spec/mailers/previews/invitation_mailer_preview.rb
+++ spec/mailers/previews/invitation_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
-
+  def demo_invite
+    InvitationMailer.demo_invite
+  end
 end
