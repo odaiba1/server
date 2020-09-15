5de894e8aefa8cea4080c6e2cb03f68eaad24590
Frances Cheng <francesvcheng@gmail.com>
2020-09-15 16:56:58 -0700

    added color and spacing between text

--- spec/mailers/previews/invitation_mailer_preview.rb
+++ spec/mailers/previews/invitation_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
   def demo_invite
-    InvitationMailer.demo_invite
+    attachments.inline['odaiba-logo_inverted_icon.png'] = File.read('/assets/images/odaiba-logo_inverted_icon.png')
   end
 end
