4dc50c571f2eea96aec7837031abe21d6992c84e
Frances Cheng <francesvcheng@gmail.com>
2020-09-15 20:54:08 -0700

    Styling of email without logo

--- spec/mailers/previews/invitation_mailer_preview.rb
+++ spec/mailers/previews/invitation_mailer_preview.rb
@@ -1,6 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
-  def demo_invite
-    attachments.inline['odaiba-logo_inverted_icon.png'] = File.read('/assets/images/odaiba-logo_inverted_icon.png')
-  end
 end
