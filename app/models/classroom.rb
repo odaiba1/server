3500831bbf76b41d6292767fed79b3fd6ec37a4d
sungmee0219 <sungmee0219@gmail.com>
2020-09-24 15:51:38 -0700

    validate start and end time so each classroom must have them

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -27,7 +27,7 @@ class Classroom < ApplicationRecord
   has_many :student_classrooms
   has_many :users, through: :student_classrooms
 
-  validates :name, presence: true
+  validates :name, :start_time, :end_time, presence: true
   validate :user_role
   validate :start_time_after_current_time
   validate :end_time_after_start_time
@@ -49,7 +49,7 @@ class Classroom < ApplicationRecord
   end
 
   def end_time_after_start_time
-    return if end_time.nil?
+    return if end_time.nil? || start_time.nil?
 
     errors.add(:end_time, ' must not be before start time') if end_time <= start_time
   end
