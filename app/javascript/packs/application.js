2e4a6e4c71a934a2ea925b1e46318aff7b467956
Paulo DAlberti <phdalberti@gmail.com>
2020-09-28 19:44:18 +0900

    flatpickr css hotfix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import "../stylesheets/application"
+import "../stylesheets/application.scss"
 import flatpickr from "flatpickr";
 
 document.addEventListener('DOMContentLoaded', function() {
