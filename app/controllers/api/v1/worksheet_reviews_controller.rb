69cb693dc506a23d262fd88337fdb24c9272fcfb
Liam Baker <liam.baker2310@gmail.com>
2020-10-14 15:03:04 +0900

    changed messages controller to worksheet_review

---
+++ app/controllers/api/v1/worksheet_reviews_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::WorksheetReviewsController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User
+
+  def create
+    @worksheet_review = WorksheetReview.new(message_params)
+    authorize @worksheet_review
+    if @worksheet_review.save
+      render json: @worksheet_review
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:worksheet_review).permit(:content, :user_id)
+  end
+
+  def render_error
+    render json: { errors: @worksheet_review.errors.full_messages },
+           status: :unprocessable_entity
+  end
+end
