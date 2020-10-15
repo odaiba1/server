c468180d7293a7319574787ba57baca0245054c0
Liam Baker <liam.baker2310@gmail.com>
2020-10-15 14:29:52 +0900

    added update and destroy to worksheet review controller

--- app/controllers/api/v1/worksheet_reviews_controller.rb
+++ app/controllers/api/v1/worksheet_reviews_controller.rb
@@ -1,12 +1,30 @@
 class Api::V1::WorksheetReviewsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User
+  before_action :set_and_authorize_worksheet_review, only: %i[update destroy]
+
 
   def create
-    @worksheet_review = WorksheetReview.new(message_params)
-    @worksheet_review.worksheet_id = params[:worksheet_id]
-    authorize @worksheet_review
-    if @worksheet_review.save
+    worksheet_review = WorksheetReview.new(worksheet_review_params)
+    worksheet_review.worksheet_id = params[:worksheet_id]
+    authorize worksheet_review
+    if worksheet_review.save
+      render json: worksheet_review
+    else
+      render_error
+    end
+  end
+
+  def update
+    if @worksheet_review.update(worksheet_review_params)
       render json: @worksheet_review
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    if @worksheet_review.destroy
+      render json: {}
     else
       render_error
     end
@@ -14,12 +32,17 @@ class Api::V1::WorksheetReviewsController < Api::V1::BaseController
 
   private
 
-  def message_params
+  def worksheet_review_params
     params.require(:worksheet_review).permit(:content, :user_id)
   end
 
   def render_error
-    render json: { errors: @worksheet_review.errors.full_messages },
+    render json: { errors: worksheet_review.errors.full_messages },
            status: :unprocessable_entity
+  end
+
+  def set_and_authorize_worksheet_review
+    @worksheet_review = WorksheetReview.find(params[:id])
+    authorize @worksheet_review
   end
 end
