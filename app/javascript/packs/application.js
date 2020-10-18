426af71864db7d97e179ee0e652c3c85b3f96560
Frances Cheng <francesvcheng@gmail.com>
2020-10-18 01:18:17 -0700

    added modal div

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -39,3 +39,6 @@ const urlField = document.querySelector("#no_model_fields_worksheet_url");
       urlField.value = item.src;
     });
   });
+
+//const showModal
+//<body onunload="myFunction()"></body>
